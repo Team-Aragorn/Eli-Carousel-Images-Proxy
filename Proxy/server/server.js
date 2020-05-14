d1e3f7212be9e65b54780083eeb14db98e63d059
Eli Caraballo <eli.caraballojr@gmail.com>
2020-05-13 21:38:50 -0500

    Finished Proxy

--- Proxy/server/server.js
+++ Proxy/server/server.js
@@ -1,71 +1,53 @@
 const express = require('express');
-const db = require('../../Recommended-Services/database/database.js');
-const db2 = require('../../Carousel-Images/database/database.js');
-const db3 = require('../../review_service/server/db/index.js');
+const axios = require('axios');
 
 const app = express();
 const PORT = 3100;
 
-app.use(express.static(`${__dirname}/../public`));
+app.use('/games/:gameId', express.static(`${__dirname}/../public`));
 app.use(express.json());
 
-app.get('/api/game', (req, res) => {
-  db.getGamesForRecommended((err, result) => {
-    if (err) {
-      res.status(404).end();
-    } else {
-      res.status(200).send(result);
-    }
-  });
+
+app.get('/carousel/:gameId', (req, res) => {
+  axios.get('/carousel/:gameId', {
+    baseURL: 'http://localhost:3003'
+  })
+    .then((data) => { console.log(data), res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.get('/api/game2', (req, res) => {
-  db2.getGameForTopCarousel((err, result) => {
-    if (err) {
-      res.status(404).end();
-    } else {
-      res.status(200).send(result);
-    }
-  });
+app.get('/recommended', (req, res) => {
+  axios.get('/recommended', {
+    baseURL: 'http://localhost:3005',
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
 app.get('/reviews/:gameId', (req, res) => {
-  db3.Review.find({ gameId: req.params.gameId }, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(result);
-    }
-  });
+  axios({
+    baseURL: `http://localhost:3002/reviews/${req.params.gameId}`
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
 
-app.post('/reviews/', jsonParser, (req, res) => {
-  db3.Review.findById(req.body.id, (err, review) => {
-    if (err) {
-      res.status(500).send(err);
-    } else if (req.body.voteString === 'yes') {
-      review.meta.helpful += 1;
-      review.save((error) => {
-        if (err) {
-          res.status(500).send(error);
-        } else {
-          res.status(202).send();
-        }
-      });
-    } else if (req.body.voteString === 'no') {
-      review.meta.unhelpful += 1;
-      review.save((error) => {
-        if (err) {
-          res.status(500).send(error);
-        } else {
-          res.status(202).send();
-        }
-      });
-    } else {
-      res.status(400).send(`"Bad Vote String: ${req.body.voteString}"`);
-    }
-  });
+app.get('/cartapi/:gameId', (req, res) => {
+  axios({
+    baseURL: `http://localhost:3001/cartapi/${req.params.gameId}`
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
 });
+
+app.get('/api/games/:gameId', (req, res) => {
+  axios({
+    baseURL: `http://localhost:3004/api/games/${req.params.gameId}`
+  })
+    .then((data) => { res.status(200).send(data.data); })
+    .catch(() => { res.status(404).end(); });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening at port:${PORT}`);
